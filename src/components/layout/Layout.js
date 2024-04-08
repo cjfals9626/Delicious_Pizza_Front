7ab956e0c1902eb87988ea707870fd6367483e67
안철민 <81482706+cjfals9626@users.noreply.github.com>
2024-04-08 19:02:27 +0900

    리액트 부트스트랩 적용 및 module.scss, 헤더 푸터 만듦

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,15 @@
-const Layout = () => {
+import layout from '../../assets/scss/layout/Layout.module.scss'
+import Header from "./header/Header"
+import Footer from "./footer/Footer"
+
+const Layout = ({children}) => {
     return (
-      <div>
-  
+      <div className={layout.layout}>
+        <Header />
+        <main>
+            {children}
+        </main>
+        <Footer />  
       </div>
     )
   }
---
+++ src/components/layout/footer/Footer.js
@@ -0,0 +1,20 @@
+
+const Footer = () => {
+    return (
+        <footer class="bg-dark">
+            <div class="w-100 bg-black py-3">
+                <div class="container">
+                    <div class="row pt-2">
+                        <div class="col-12">
+                            <p class="text-left text-light">
+                                Copyright &copy; CM
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+  }
+  
+  export default Footer
